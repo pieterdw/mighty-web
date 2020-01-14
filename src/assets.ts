6c8e1d5404338dd63f1809b9d655b5db2d39de18
Pieter De Witte <pieter@straffekoffie.com>
2020-01-14 20:08:11 +0100

    Version number replacement

--- src/assets.ts
+++ src/assets.ts
@@ -1,6 +1,7 @@
 const { spawn } = require("child_process");
 import { sync as del } from "del";
 import { ncp } from "ncp";
+const fs = require("fs");
 
 export const copyAssetsToDist = (emptyFolderFirst: boolean) => {
   if (emptyFolderFirst) {
@@ -13,7 +14,38 @@ export const copyAssetsToDist = (emptyFolderFirst: boolean) => {
         reject(err);
       }
       console.log("Done syncing public folder to dist folder.");
+      setVersionNumber()
+        .then(resolve)
+        .catch(reject);
       resolve();
     });
+  });
+};
+
+const setVersionNumber = () => {
+  const functionsFileName = ".\\dist\\functions.php";
+  const versionNumber = Math.round(Math.random() * 100000) + "";
+  return new Promise((resolve, reject) => {
+    if (fs.existsSync(functionsFileName)) {
+      fs.readFile(functionsFileName, "utf8", (err: any, data: string) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+          return;
+        }
+        var result = data.split("{{versionNumber}}").join(versionNumber);
+        fs.writeFile(functionsFileName, result, "utf8", (err: any) => {
+          if (err) {
+            console.error(err);
+            reject(err);
+            return;
+          }
+          console.log("Done setting version number");
+          resolve();
+        });
+      });
+    } else {
+      resolve();
+    }
   });
 };
